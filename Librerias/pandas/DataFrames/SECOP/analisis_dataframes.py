ccce4a9f914471e31897b2fea68028aa5e02692a
Joan Sebastián Carrillo Barón <sebastian.carrillob01@gmail.com>
2022-05-14 21:13:07 -0500

    Feat: Analisis Dataframes

---
+++ Librerias/pandas/DataFrames/SECOP/analisis_dataframes.py
@@ -0,0 +1,8 @@
+"""
+funcion head(n) -> Top n numero de datos
+funcion tail(n) -> De abajo n numero de datos
+info() -> Tipo de dato, non null objetct
+Dtyooe
+describe() -> count, unique, top, freq,first, last
+"""
+
